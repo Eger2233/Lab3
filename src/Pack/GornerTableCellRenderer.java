47589330763e7130611b9d409e46aa920074f46b
Eger2233 <funtik4433@gmail.com>
2020-12-08 01:02:31 +0300

    Commit Changes
    commit changes
    commit messsage
    commit changes

---
+++ src/Pack/GornerTableCellRenderer.java
@@ -0,0 +1,91 @@
+package Pack;
+
+import java.awt.Color;
+import java.awt.Component;
+import java.awt.FlowLayout;
+import java.text.DecimalFormat;
+import java.text.DecimalFormatSymbols;
+import java.text.NumberFormat;
+import javax.swing.*;
+import javax.swing.table.TableCellRenderer;
+public class GornerTableCellRenderer implements TableCellRenderer {
+    private JPanel panel = new JPanel();
+    private JLabel label = new JLabel();
+    // Ищем ячейки, строковое представление которых совпадает с needle
+// (иголкой). Применяется аналогия поиска иголки в стоге сена, в роли
+// стога сена - таблица
+    private String needle = null;
+    private Boolean Palindromes = false;
+    private DecimalFormat formatter = (DecimalFormat)NumberFormat.getInstance();
+    public GornerTableCellRenderer() {
+// Показывать только 10 знаков после запятой
+        formatter.setMaximumFractionDigits(10);
+// Не использовать группировку (т.е. не отделять тысячи
+// ни запятыми, ни пробелами), т.е. показывать число как "1000",
+// а не "1 000" или "1,000"
+        formatter.setGroupingUsed(false);
+// Установить в качестве разделителя дробной части точку, а не
+// запятую. По умолчанию, в региональных настройках
+// Россия/Беларусь дробная часть отделяется запятой
+        DecimalFormatSymbols dottedDouble = formatter.getDecimalFormatSymbols();
+        dottedDouble.setDecimalSeparator('.');
+        formatter.setDecimalFormatSymbols(dottedDouble);
+// Разместить надпись внутри панели
+        panel.add(label);
+// Установить выравнивание надписи по левому краю панели
+        panel.setLayout(new FlowLayout(FlowLayout.LEFT));
+    }
+    public Component getTableCellRendererComponent(JTable table, Object value, boolean isSelected, boolean hasFocus, int row, int col) {
+// Преобразовать double в строку с помощью форматировщика
+        String formattedDouble = formatter.format(value);
+// Установить текст надписи равным строковому представлению числа
+
+        if (col==1 && needle!=null && needle.equals(formattedDouble)) {
+// Номер столбца = 1 (т.е. второй столбец) + иголка не null
+// (значит что-то ищем) +
+// значение иголки совпадает со значением ячейки таблицы -
+// то флажок
+            JCheckBox Check = new JCheckBox();
+            Check.setBackground(Color.WHITE);
+            Check.doClick();
+            return Check;
+        }
+        else if(Palindromes){
+            String word = "";
+            Boolean TruePalindrom = true;
+            for(int i = 0 ; i<formattedDouble.length();i++){
+                if(formattedDouble.charAt(i)=='.'){
+
+                }
+                else{
+                    word+=formattedDouble.charAt(i);
+                }
+
+            }
+            for(int i = 0 ; i<word.length();i++){
+                if(word.charAt(i)!=word.charAt(word.length()-1-i)){
+                    TruePalindrom = false;
+                    break;
+                }
+            }
+            if(TruePalindrom){
+                panel.setBackground(Color.RED);
+            }
+            else{
+                panel.setBackground(Color.WHITE);
+            }
+        }
+        else {
+// Иначе - в обычный белый
+            panel.setBackground(Color.WHITE);
+        }
+        label.setText(formattedDouble);
+        return panel;
+    }
+    public void setNeedle(String needle) {
+        this.needle = needle;
+    }
+    public void setPalindromes(Boolean Palindromes) {
+        this.Palindromes = Palindromes;
+    }
+}
---
+++ src/Pack/GornerTableModel.java
@@ -0,0 +1,84 @@
+package Pack;
+
+import javax.swing.table.AbstractTableModel;
+@SuppressWarnings("serial")
+public class GornerTableModel extends AbstractTableModel {
+    private Double[] coefficients;
+    private Float[] coeff;
+    private Double from;
+    private Double to;
+    private Double step;
+    private Double result1;
+    private Float result2;
+    private Double result3;
+
+    public GornerTableModel(Double from, Double to, Double step, Double[] coefficients, Float[] coeff) {
+        this.from = from;
+        this.to = to;
+        this.step = step;
+        this.coefficients = coefficients;
+        this.coeff = coeff;
+    }
+    public Double getFrom() {
+        return from;
+    }
+    public Double getTo() {
+        return to;
+    }
+    public Double getStep() {
+        return step;
+    }
+    public int getColumnCount() {
+// В данной модели 4 столбца
+        return 4;
+    }
+    public int getRowCount() {
+// Вычислить количество точек между началом и концом отрезка
+// исходя из шага табулирования
+        return new Double(Math.ceil((to-from)/step)).intValue()+1;
+    }
+    public Object getValueAt(int row, int col) {
+// Вычислить значение X как НАЧАЛО_ОТРЕЗКА + ШАГ*НОМЕР_СТРОКИ
+        double x = from + step*row;
+        switch (col){
+            case 0:
+                return x;
+            case 1:
+            {
+                result1 = coefficients[coefficients.length-1];
+                for(int i = coefficients.length-1; i > 0; i--){
+                    result1 = result1*x+coefficients[i-1];
+                }
+                return result1;
+            }
+            case 2:
+            {
+                result2 = coeff[coeff.length-1];
+                for(int i = coeff.length-1; i > 0; i--){
+                    result2 = result2*(float)x+coeff[i-1];
+                }
+                return result2;
+            }
+            default:
+                return result3 = result2 - result1;
+        }
+
+    }
+    public String getColumnName(int col) {
+        switch (col){
+            case 0:
+                return "Значение X";
+            case 1:
+                return "Значение многочлена";
+            case 2:
+                return "Через float";
+            default:
+                return "Разница";
+        }
+    }
+    public Class<?> getColumnClass(int col) {
+
+        return Double.class;
+
+    }
+}
